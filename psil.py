4716ec450f5bd96d05a09bea6b6c56d17f8673a7
Greg Hewgill <greg@hewgill.com>
2008-11-29 17:52:57 +1300

    remove math dependency

--- psil.py
+++ psil.py
@@ -11,7 +11,6 @@
 
 """
 
-import math
 import re
 
 RE_NUMBER = re.compile(r"[-+]?\d+(\.\d+)?(e[-+]?\d+)?", re.IGNORECASE)
@@ -94,8 +93,6 @@ def tokenise(s):
             if m:
                 if m.group(1) or m.group(2):
                     x = float(m.group(0))
-                    if x == math.floor(x):
-                        x = int(x)
                     yield (Token.NUMBER, x)
                 else:
                     yield (Token.NUMBER, int(m.group(0)))
@@ -359,28 +356,6 @@ Globals.symbols["gcd"]       = lambda x, y: x if y == 0 else Globals.symbols["gc
 Globals.symbols["lcm"]       = lambda x, y: x * y # TODO
 Globals.symbols["numerator"] = lambda x: x # TODO
 Globals.symbols["denominator"] = lambda x: x # TODO
-Globals.symbols["floor"]     = lambda x: int(math.floor(x)) if not isinstance(x, int) else x
-Globals.symbols["ceiling"]   = lambda x: int(math.ceil(x)) if not isinstance(x, int) else x
-Globals.symbols["truncate"]  = lambda x: int(math.ceil(x)) if x < 0 else int(math.floor(x))
-Globals.symbols["round"]     = lambda x: int(math.floor(x + 0.5)) if not isinstance(x, int) else x
-Globals.symbols["exp"]       = math.exp
-Globals.symbols["log"]       = math.log
-Globals.symbols["sin"]       = math.sin
-Globals.symbols["cos"]       = math.cos
-Globals.symbols["tan"]       = math.tan
-Globals.symbols["asin"]      = math.asin
-Globals.symbols["acos"]      = math.acos
-Globals.symbols["atan"]      = lambda *args: math.atan(args[0]) if len(args) == 1 else math.atan2(args[0], args[1])
-Globals.symbols["sqrt"]      = math.sqrt
-Globals.symbols["expt"]      = math.pow
-Globals.symbols["make-rectangular"] = lambda x, y: 0 # TODO
-Globals.symbols["make-polar"] = lambda x, y: 0 # TODO
-Globals.symbols["real-part"]  = lambda z: 0 # TODO
-Globals.symbols["imag-part"]  = lambda z: 0 # TODO
-Globals.symbols["magnitude"]  = lambda z: 0 # TODO
-Globals.symbols["angle"]      = lambda z: 0 # TODO
-Globals.symbols["exact->inexact"] = lambda x: x # TODO
-Globals.symbols["inexact->exact"] = lambda x: x # TODO
 Globals.symbols["number->string"] = lambda x, b = 10: str(x) if b == 10 else hex(x)[2:] # TODO
 Globals.symbols["string->number"] = lambda x, b = 10: int(x, b) # TODO
 
