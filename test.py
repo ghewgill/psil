1ac3abd484f04cb75c41c12b337ba910e833022d
Greg Hewgill <greg@hewgill.com>
2009-08-16 17:18:40 +1200

    update test program

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ fact = psil("""
     (lambda (x)
       (if (== x 1)
           1
-          (* x (fact (- x 1)))))""", globals = globals())
+          (* x (fact (- x 1)))))""", glob = globals())
 
 print fact(5)
 
 sq = lambda x: x * x
-print psil("(sq 5)", globals = globals())
-foo = psil("(lambda (x) (sq x))", globals = globals())
+print psil("(sq 5)", glob = globals())
+foo = psil("(lambda (x) (sq x))", glob = globals())
 print foo(4)
 
 print psil("(import os) (getattr os \"name\")")
