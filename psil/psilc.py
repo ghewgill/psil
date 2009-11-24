0ec746d4813b73e4ff766e7855aabf1df88d30bd
Greg Hewgill <greg@hewgill.com>
2009-11-24 22:24:27 +1300

    instantiate expr_context nodes

--- psil/psilc.py
+++ psil/psilc.py
@@ -26,7 +26,7 @@ def compile_define(p):
             stmt[-1] = ast.Return(stmt[-1])
         return ast.FunctionDef(pydent(p[1][0].name), [x.name for x in p[1][1:]], ast.Suite(stmt), None, None)
     else:
-        return ast.Assign([ast.Name(pydent(p[1].name), ast.Store)], build_ast(p[2]))
+        return ast.Assign([ast.Name(pydent(p[1].name), ast.Store())], build_ast(p[2]))
 
 def compile_divide(p):
     if len(p) == 2:
@@ -64,7 +64,7 @@ def compile_multiply(p):
 def compile_quote(p):
     def q(p):
         if isinstance(p, list):
-            return ast.List([q(x) for x in p], ast.Load)
+            return ast.List([q(x) for x in p], ast.Load())
         elif isinstance(p, Symbol):
             return compiler.ast.CallFunc(compiler.ast.Name("sym"), [compiler.ast.Const(p.name)])
         else:
@@ -100,28 +100,28 @@ CompileFuncs = {
     Symbol.new("is"): lambda p: ast.BinOp(build_ast(p[1]), ast.Is(), build_ast(p[2])),
     Symbol.new("is-not"): lambda p: ast.BinOp(build_ast(p[1]), ast.IsNot(), build_ast(p[2])),
     Symbol.new("define"): compile_define,
-    Symbol.new("dict-set"): lambda p: ast.Assign([ast.Subscript(build_ast(p[1]), ast.Index(build_ast(p[2])), ast.Store)], build_ast(p[3])),
+    Symbol.new("dict-set"): lambda p: ast.Assign([ast.Subscript(build_ast(p[1]), ast.Index(build_ast(p[2])), ast.Store())], build_ast(p[3])),
     #Symbol.new("caadr"): lambda p: compiler.ast.Subscript(compiler.ast.Subscript(build_ast(p[1]), 0, compiler.ast.Const(1)), 0, compiler.ast.Const(0)),
-    Symbol.new("caar"): lambda p: ast.Subscript(ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(0)), ast.Load), ast.Index(ast.Num(0)), ast.Load),
+    Symbol.new("caar"): lambda p: ast.Subscript(ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(0)), ast.Load()), ast.Index(ast.Num(0)), ast.Load()),
     #Symbol.new("cadddr"): lambda p: compiler.ast.Subscript(build_ast(p[1]), 0, compiler.ast.Const(3)),
     #Symbol.new("caddr"): lambda p: compiler.ast.Subscript(build_ast(p[1]), 0, compiler.ast.Const(2)),
-    Symbol.new("cadr"): lambda p: ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(1)), ast.Load),
-    Symbol.new("car"): lambda p: ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(0)), ast.Load),
-    Symbol.new("cdar"): lambda p: ast.Subscript(ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(0)), ast.Load), ast.Slice(ast.Num(1), None, None), ast.Load),
+    Symbol.new("cadr"): lambda p: ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(1)), ast.Load()),
+    Symbol.new("car"): lambda p: ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(0)), ast.Load()),
+    Symbol.new("cdar"): lambda p: ast.Subscript(ast.Subscript(build_ast(p[1]), ast.Index(ast.Num(0)), ast.Load()), ast.Slice(ast.Num(1), None, None), ast.Load()),
     #Symbol.new("cddr"): lambda p: compiler.ast.Slice(build_ast(p[1]), 0, compiler.ast.Const(2), None),
-    Symbol.new("cdr"): lambda p: ast.Subscript(build_ast(p[1]), ast.Slice(ast.Num(1), None, None), ast.Load),
-    Symbol.new("cons"): lambda p: ast.BinOp(ast.List([build_ast(p[1])], ast.Load), ast.Add(), build_ast(p[2])),
+    Symbol.new("cdr"): lambda p: ast.Subscript(build_ast(p[1]), ast.Slice(ast.Num(1), None, None), ast.Load()),
+    Symbol.new("cons"): lambda p: ast.BinOp(ast.List([build_ast(p[1])], ast.Load()), ast.Add(), build_ast(p[2])),
     Symbol.new("apply"): lambda p: ast.Call(build_ast(p[1]), None, None, build_ast(p[2]), None),
     Symbol.new("if"): lambda p: ast.IfExp(build_ast(p[1]), build_ast(p[2]), build_ast(p[3]) if len(p) >= 4 else None),
     Symbol.new("in"): lambda p: ast.BinOp(build_ast(p[1]), ast.In(), build_ast(p[2])),
-    Symbol.new("index"): lambda p: ast.Subscript(build_ast(p[1]), ast.Index(build_ast(p[2])), ast.Load),
+    Symbol.new("index"): lambda p: ast.Subscript(build_ast(p[1]), ast.Index(build_ast(p[2])), ast.Load()),
     Symbol.new("lambda"): compile_lambda,
-    Symbol.new("list"): lambda p: ast.List([build_ast(x) for x in p[1:]], ast.Load),
+    Symbol.new("list"): lambda p: ast.List([build_ast(x) for x in p[1:]], ast.Load()),
     Symbol.new("not"): lambda p: ast.UnaryOp(ast.Not(), build_ast(p[1])),
     Symbol.new("not-in"): lambda p: ast.BinOp(build_ast(p[1]), ast.NotIn(), build_ast(p[2])),
     Symbol.new("quote"): compile_quote,
-    Symbol.new("set!"): lambda p: ast.Assign([ast.Name(p[1].name, ast.Store)], build_ast(p[2])),
-    Symbol.new("slice"): lambda p: ast.Subscript(build_ast(p[1]), ast.Slice(build_ast(p[2]), build_ast(p[3]), None), ast.Load),
+    Symbol.new("set!"): lambda p: ast.Assign([ast.Name(p[1].name, ast.Store())], build_ast(p[2])),
+    Symbol.new("slice"): lambda p: ast.Subscript(build_ast(p[1]), ast.Slice(build_ast(p[2]), build_ast(p[3]), None), ast.Load()),
     Symbol.new("string->symbol"): lambda p: ast.Call(ast.Name("intern"), [build_ast(p[1])], None, None, None),
 }
 
@@ -136,13 +136,13 @@ def build_ast(p, tail = False):
             if f:
                 return f(p)
             elif p[0].name.startswith("."):
-                return ast.Call(ast.Attribute(build_ast(p[1]), p[0].name[1:], ast.Load), [build_ast(x) for x in p[2:]], None, None, None)
+                return ast.Call(ast.Attribute(build_ast(p[1]), p[0].name[1:], ast.Load()), [build_ast(x) for x in p[2:]], None, None, None)
             else:
-                return ast.Call(ast.Name(pydent(p[0].name), ast.Load), [build_ast(x) for x in p[1:]], None, None, None)
+                return ast.Call(ast.Name(pydent(p[0].name), ast.Load()), [build_ast(x) for x in p[1:]], None, None, None)
         else:
             return ast.Call(build_ast(p[0]), [build_ast(x) for x in p[1:]], None, None, None)
     elif isinstance(p, Symbol):
-        return ast.Name(pydent(p.name), ast.Load)
+        return ast.Name(pydent(p.name), ast.Load())
     else:
         return ast.Num(p)
 
