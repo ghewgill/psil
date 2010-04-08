4eb7f4178a40ca3ce0fb547fafb60d9077f0f517
Greg Hewgill <greg@hewgill.com>
2010-04-09 06:50:25 +1200

    update print statements for python 3

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ fact = psil("""
           1
           (* x (fact (- x 1)))))""", glob = globals())
 
-print fact(5)
+print(fact(5))
 
 sq = lambda x: x * x
-print psil("(sq 5)", glob = globals())
+print(psil("(sq 5)", glob = globals()))
 foo = psil("(lambda (x) (sq x))", glob = globals())
-print foo(4)
+print(foo(4))
 
-print psil("(import os) (getattr os \"name\")")
+print(psil("(import os) (getattr os \"name\")"))
